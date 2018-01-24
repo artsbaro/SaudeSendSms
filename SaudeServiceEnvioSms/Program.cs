f6ae62d9b62a55745f026069d8ae462322d53f38
DESKTOP-GCPG4PV\ANTONIO-PC <artsbaro@hotmail.com>
2018-01-23 23:02:32 -0200

    Add project files.

---
+++ SaudeServiceEnvioSms/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaudeServiceEnvioSms
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main()
+        {
+
+
+
+
+
+            // Não é necessário instalar o Windows Service para testes!
+            // Bastar setar a solution em modo Debug
+            // E configurar os dois projetos para inicializarem juntos
+
+#if (!DEBUG)
+                        ServiceBase[] ServicesToRun;
+                        ServicesToRun = new ServiceBase[] 
+                        { 
+                            new SendSms() 
+                        };
+                        ServiceBase.Run(ServicesToRun);
+#else
+            var service = new SendSms();
+            // Chamada do seu método para Debug.
+            service.Initialize();
+            // Coloque sempre um breakpoint para o ponto de parada do seu código.
+            //System.Threading.Thread.Sleep(System.Threading.Timeout.Infinite);
+#endif
+
+
+
+
+
+
+
+
+
+            //Outro Servico aqui
+            //ServiceBase[] ServicesToRun;
+            //ServicesToRun = new ServiceBase[]
+            //{
+            //    new SendSms(),
+            //};
+            //ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
---
+++ SaudeServiceEnvioSms/SendSms.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Configuration;
+using System.ServiceProcess;
+using System.Threading;
+
+namespace SaudeServiceEnvioSms
+{
+    partial class SendSms : ServiceBase
+    {
+        public SendSms()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Initialize();
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+        }
+
+        public void InitializeSend()
+        {
+            while (true)
+            {
+                int time = 600000;
+                var prm = ConfigurationManager.AppSettings["IntervaloDeTempoParaExecucao"];
+                if (prm?.Length > 0)
+                    time = Convert.ToInt32(prm);
+
+                Thread.Sleep(time);
+                Process();
+            }
+        }
+
+        public void Initialize()
+        {
+            ThreadStart start = new ThreadStart(InitializeSend);
+            Thread thread = new Thread(start);
+
+            thread.Start();
+        }
+
+        private void SendSmsMethod()
+        {
+            double timeInHours = 12;
+            var prm = ConfigurationManager.AppSettings["TempoEmHorasAnteriorAConsultaParaEnvioDeSms"];
+            if (prm?.Length > 0)
+                timeInHours = Convert.ToDouble(prm);
+
+            var dateCut = DateTime.Now.AddHours(timeInHours);
+
+            /*
+              1 - Montar Lista de Consultas Médicas com Data da Consulta (PATC_D_DATA) inferior a dateCut
+               1.1 Consulta Não efetuada
+               1.2 Paciente existente e ativo
+               1.3 Telefone do paciente Válido
+
+              2 -  Montar Json com os Sms a serem enviados
+               
+              3 - Enviar SMS  
+
+             */
+        }
+
+        private void Process()
+        {
+            DateTime horaInicialExecucao = DateTime.MinValue;
+            var prm = ConfigurationManager.AppSettings["ExecucaoPeriodoInicial"];
+            if (prm?.Length > 0)
+                horaInicialExecucao = Convert.ToDateTime($"{DateTime.Today:yyyy-MM-dd}" + " " + prm);
+
+            DateTime horaFinalExecucao = DateTime.MinValue;
+            prm = ConfigurationManager.AppSettings["ExecucaoPeriodoFinal"];
+            if (prm?.Length > 0)
+                horaFinalExecucao = Convert.ToDateTime($"{DateTime.Today:yyyy-MM-dd}" + " " + prm);
+
+            if (DateTime.Now >= horaInicialExecucao && DateTime.Now <= horaFinalExecucao)
+            {
+                SendSmsMethod();
+            }
+        }
+    }
+}
